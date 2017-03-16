fb1c915c7ed640b777169524888eb2b7c0a2ed92
Mahammad8564 <mahammadc_productivet@outlook.com>
2017-03-16 13:49:08 +0530

    asdfgh

---
+++ app/routes/order.js
@@ -0,0 +1,16 @@
+﻿
+var order = require('../../app/controllers/order');
+var queryBuilder = require('../../app/helper/queryBuilder');
+
+module.exports = function (app) {
+    app.route('/api/order')
+        .get(queryBuilder.queryBuilder, order.list)
+        .post(order.create);
+
+    app.route('/api/order/:orderId')
+        .get(order.read)
+        .patch(order.update)
+        //.delete(measurement.delete);
+
+    app.param('orderId', order.getById);
+}
---
+++ app/routes/orderItem.js
@@ -0,0 +1,16 @@
+﻿
+var orderItem = require('../../app/controllers/orderItem');
+var queryBuilder = require('../../app/helper/queryBuilder');
+
+module.exports = function (app) {
+    app.route('/api/orderItem')
+        .get(queryBuilder.queryBuilder, orderItem.list)
+        .post(orderItem.create);
+
+    app.route('/api/orderItem/:orderItemId')
+        .get(orderItem.read)
+        .patch(orderItem.update)
+        //.delete(measurement.delete);
+
+    app.param('orderItemId', orderItem.getById);
+}
