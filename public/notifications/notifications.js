667590df13c6b85915e70efc808b973664723c90
Mahammad8564 <mahammadc_productivet@outlook.com>
2017-03-17 12:29:14 +0530

    images

--- public/notifications/notifications.js
+++ public/notifications/notifications.js
@@ -9,6 +9,19 @@
 
     function NotificationsController(Restangular, $state) {
         var vm = this;
+        var today = new Date();
+        Restangular.all('api/customer').getList().then(function (res) {
+            // vm.list = res.data;
+            // vm.options.totalItems = parseInt(res.headers('total'));
+            console.log(res.data[0].dob);
+            console.log(res.data[0].annerversary);
+            if(res.data[0].dob == today){
+              vm.birthday = true;
+            }
+            if(res.data[0].annerversary == today){
+              vm.annerversary = true;
+            }
+        });
 
     }
 
